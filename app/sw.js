1250abd8781a298f9d3ed98183bd8d36324af98a
Ant Motors <antmotors@local>
2026-08-06 18:44:25 +0000

    fix(i18n): top auth button now follows language via applyI18n (early path); setLang runs updateAuthCTA first; SW v29

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'antmotors-v28';
+const CACHE = 'antmotors-v29';
 const STATIC = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon.svg'];
 
 self.addEventListener('install', e => {
