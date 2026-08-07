24d5f31b4a234b50cf7295c5bd22789f77e6b232
Ant Motors <antmotors@local>
2026-08-07 18:22:13 +0000

    fix: hide redundant sec-head titles on sub-pages (fixed sub-bar already shows them); SW v47

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'antmotors-v46';
+const CACHE = 'antmotors-v47';
 const STATIC = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon.svg'];
 
 self.addEventListener('install', e => {
