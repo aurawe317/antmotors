eb445b074fed8792aa6fa1296341aab5fc458f8c
Ant Motors <antmotors@local>
2026-08-07 18:26:41 +0000

    fix: hide ALL redundant title rows (sec-head + admin-head) on sub-pages; add 44px padding-top so content clears fixed bar; detail-top pushed down not hidden; SW v48

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'antmotors-v47';
+const CACHE = 'antmotors-v48';
 const STATIC = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon.svg'];
 
 self.addEventListener('install', e => {
