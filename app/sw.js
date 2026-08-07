2e670585fcd5bcf745efaa5d8824941a1da8a653
Ant Motors <antmotors@local>
2026-08-07 20:18:32 +0000

    ui: merge company + membership into one block; visible only to boss+partners (isTopRole); SW v55

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'antmotors-v54';
+const CACHE = 'antmotors-v55';
 const STATIC = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon.svg'];
 
 self.addEventListener('install', e => {
