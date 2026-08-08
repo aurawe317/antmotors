d7a7c4478f3fafd0e1d8751699300ffd24a931ca
Ant Motors <antmotors@local>
2026-08-08 23:48:17 +0000

    fix(v71): replace floating detail-top action row with sub-bar '⋯' menu (Save/Share/Theme/Edit); eliminates overlap flicker + 'buttons jump right-to-left' on tap; menu shown only on detail page

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'antmotors-v70';
+const CACHE = 'antmotors-v71';
 const STATIC = ['./', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon.svg'];
 
 self.addEventListener('install', e => {
