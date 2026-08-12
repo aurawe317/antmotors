c76796b817f6688f8af379a80048d91ff5c62a30
Ant Motors <antmotors@local>
2026-08-12 22:27:17 +0000

    v117: fix crop producing empty 'data:,' images ('?'), album-style pinch/pan crop, strip fake Kwame fallback + broken photos

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'antmotors-v116';
+const CACHE = 'antmotors-v117';
 const STATIC = ['./', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon.svg'];
 
 // Allow the page to force this worker to take over immediately (used by the
