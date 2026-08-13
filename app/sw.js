41811d9a6ce253bd478c259ee6b12b875c094ab8
Ant Motors <antmotors@local>
2026-08-13 18:49:35 +0000

    v120: stop auto-pull failure from wiping local inventory, harden session restore, fix employee-delete not_found (purge stale ids), add missing zh/en delete i18n keys, default UI language to Chinese

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'antmotors-v119';
+const CACHE = 'antmotors-v120';
 const STATIC = ['./', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon.svg'];
 
 // Allow the page to force this worker to take over immediately (used by the
