aa099868b6dc97dd2e83deaa27e98a430cfcabce
Ant Motors <antmotors@local>
2026-08-14 16:21:42 +0000

    v124: kill 'boss' ghost (am_session is sole source of truth; purge legacy am_me='boss'); boot with metadata-only pull so cars render in seconds, photos stream in background

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'antmotors-v123';
+const CACHE = 'antmotors-v124';
 const STATIC = ['./', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon.svg'];
 
 // Allow the page to force this worker to take over immediately (used by the
